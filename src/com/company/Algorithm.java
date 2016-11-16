934ab1a66b811abb24f6de193f7d19121856fa8f
DmitryO <ordyntcev.dmitry@e-queo.com>
2016-11-16 15:35:20 +0300

    fa

---
+++ src/com/company/Algorithm.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by Dmitry on 16.11.2016.
+ */
+public class Algorithm {
+
+    public static void algorithms (String[] args) {
+
+
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,9 +21,16 @@ public  class Main {
 
     public static void main(String[] args) throws Exception {
 
-        if (args.length > 0) {
-            String s = args[0];
-            String textfile =s;
+
+
+        if (args.length < 3) {
+            System.out.println("Недостаточно агрументов");
+        }
+
+        String textfile = args[0];
+        String format  = args[1];
+        String typeSort  = args[2];
+
 
             ReadJSON readJSON = new ReadJSON();
             ReadFile readFile = new ReadFile();
@@ -36,16 +43,16 @@ public  class Main {
 
             if (textfile.equals("test.txt")) {
                 System.out.println("Введите тип сортировки");
-                String typeSort = in.next();
+                System.out.println(typeSort);
                 int[] a = new int[]{};
-                a = readArrayInFile.readArr(s);
+                a = readArrayInFile.readArr(textfile);
                 System.out.print("Maccив: ");
                 System.out.println(Arrays.toString(a));
                 System.out.print("Отсортированный массив: ");
                 getTypeSorting.getTypeSorting(typeSort, a);
             } else if (textfile.equals("test1.txt")) {
                 System.out.println("Введите тип сортировки");
-                String typeSort = in.next();
+                System.out.println(typeSort);
                 int[] a = new int[]{};
                 resStr = readFile.read();
                 System.out.print("Maccив: ");
@@ -59,4 +66,4 @@ public  class Main {
 
         }
     }
-}
+
