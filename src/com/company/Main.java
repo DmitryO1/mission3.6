eadf5cd0058f0713fe23f3cff3d77c456dfa73e4
DmitryO <ordyntcev.dmitry@e-queo.com>
2016-11-16 15:03:40 +0300

    Fixed3.6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,47 +15,48 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
+import java.io.File;
 
 public  class Main {
 
     public static void main(String[] args) throws Exception {
 
-        BubleSort bubleSort = new BubleSort();
-        InserSort inserSort = new InserSort();
-        MergeSort mergeSort = new MergeSort();
-        ReadJSON readJSON = new ReadJSON();
-        ReadFile readFile = new ReadFile();
-        GetTypeSorting getTypeSorting = new GetTypeSorting();
-        ReadArrayInFile readArrayInFile = new ReadArrayInFile();
-        GetTypeSorting sorting = new GetTypeSorting();
-        Gson parser = new Gson();
-        String resStr;
+        if (args.length > 0) {
+            String s = args[0];
+            String textfile =s;
 
-        Scanner in = new Scanner(System.in);
+            ReadJSON readJSON = new ReadJSON();
+            ReadFile readFile = new ReadFile();
+            GetTypeSorting getTypeSorting = new GetTypeSorting();
+            ReadArrayInFile readArrayInFile = new ReadArrayInFile();
+            Gson parser = new Gson();
+            String resStr;
+            Scanner in = new Scanner(System.in);
 
-        String textfile = in.next();
-        if (textfile.equals("test.txt")) {
-            String typeSort = in.next();
-            int[] a = new int[]{};
-            a = readArrayInFile.readArr("test.txt");
-            System.out.println(Arrays.toString(a));
-            getTypeSorting.getTypeSorting(typeSort, a);
+
+            if (textfile.equals("test.txt")) {
+                System.out.println("Введите тип сортировки");
+                String typeSort = in.next();
+                int[] a = new int[]{};
+                a = readArrayInFile.readArr(s);
+                System.out.print("Maccив: ");
+                System.out.println(Arrays.toString(a));
+                System.out.print("Отсортированный массив: ");
+                getTypeSorting.getTypeSorting(typeSort, a);
+            } else if (textfile.equals("test1.txt")) {
+                System.out.println("Введите тип сортировки");
+                String typeSort = in.next();
+                int[] a = new int[]{};
+                resStr = readFile.read();
+                System.out.print("Maccив: ");
+                System.out.println(resStr);
+
+                int[] arr = readJSON.toArray(resStr, parser);//Парсинг в массив
+                System.out.print("Отсортированный массив: ");
+                getTypeSorting.getTypeSorting(typeSort, arr);
+            } else System.out.println("Файл не найден");
+
+
         }
-
-        else if (textfile.equals("test1.txt")){
-            String typeSort = in.next();
-            int[] a = new int[]{};
-            resStr = readFile.read();
-            System.out.println(resStr);
-
-            int[] arr = readJSON.toArray(resStr, parser);//Парсинг в массив
-            getTypeSorting.getTypeSorting(typeSort, arr);
-        }
-
-        else System.out.println("Файл не найден");
-
-
-
     }
 }
